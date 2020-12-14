fb4c75785f15eca0f085fab6f8b7eb13922c7da8
itzik-dan <tsahidan@tsahis-MacBook-Air.local>
2020-12-14 14:29:27 +0200

    changing to celebrity recognition

--- src/App.js
+++ src/App.js
@@ -1,82 +1,94 @@
-import React, { useState } from 'react';
-import AddImage from './components/AddImage/AddImage'
-import ModelImage from './components/ModelImage/ModelImage'
-import './App.css';
-import Popup from './components/Popup/Popup';  
-import Spinner from './components/Spinner/Spinner';  
-import Clarifai from 'clarifai';
+import React, { useState } from "react";
+import AddImage from "./components/AddImage/AddImage";
+import ModelImage from "./components/ModelImage/ModelImage";
+import "./App.css";
+import Popup from "./components/Popup/Popup";
+import Spinner from "./components/Spinner/Spinner";
+import Clarifai from "clarifai";
 
 //Initialize machine learning api
 const app = new Clarifai.App({
- apiKey: 'c18d80e9c3554eb4aaa7c321034222dd'
+  apiKey: "42203fad1aa34068b903fe77f8cbc4d2",
 });
 
 function App() {
-
   //Initialize the image input field
-  const [input, setInput] = useState('')
-  const [imgUrl, setImgUrl] = useState('')
+  const [input, setInput] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
   //Initialize the face recognition frame
-  const [box, setBox] = useState([])
-   //Initialize the predictions output array from api
-  const [predictions, setPredictions] = useState([])
+  const [box, setBox] = useState([]);
+  //Initialize the predictions output array from api
+  const [predictions, setPredictions] = useState([]);
   //Initialize the loading spinner, will be used once api is loading the face predictions
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   //function for detecting the face and frame it
-  const facePosition = data => {
+  const facePosition = (data) => {
     const boundaries = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('img')
-    const width = Number(image.width)
-    const height = Number(image.height)
+    const image = document.getElementById("img");
+    const width = Number(image.width);
+    const height = Number(image.height);
     return {
-      leftCol: boundaries.left_col*width,
-      topRow: boundaries.top_row*height,
-      rightCol: width - (boundaries.right_col*width),
-      bottomRow: height - (boundaries.bottom_row*height)
-    }
-  }
+      leftCol: boundaries.left_col * width,
+      topRow: boundaries.top_row * height,
+      rightCol: width - boundaries.right_col * width,
+      bottomRow: height - boundaries.bottom_row * height,
+    };
+  };
 
   //function for updating the image url input field
-  const handleInputChange = event => {
-    setInput(event.target.value)
-  }   
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
 
-  //function for predicting the face using the api, will trigger when "Add Face" button is triggered
+  //function for predicting the face using the api, will trigger when "Predict" button is triggered
   const onSubmit = () => {
-    setImgUrl(input)
-    setLoading(true)
-    app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', input)
-    .then(response => {
-      const predict_arr = []
-      response.outputs[0].data.regions[0].data.concepts.map((el, index) => {
-        return index === 0 || index === 20 || index === 22 ? predict_arr.push(el) : null
+    setImgUrl(input);
+    setLoading(true);
+    app.models
+      .predict("e466caa0619f444ab97497640cefc4dc", input)
+      .then((response) => {
+        console.log(response);
+        const predict_arr = [];
+        response.outputs[0].data.regions[0].data.concepts.map((el, index) => {
+          return index === 0 || index === 1 || index === 2
+            ? predict_arr.push(el)
+            : null;
+        });
+        setPredictions(predict_arr);
+        setBox(facePosition(response));
       })
-      setPredictions(predict_arr)
-      setBox(facePosition(response))
-  })
-    .catch(err => {
-      let r = window.confirm("Pictue is too blurry, press 'OK' to add new vivid picture." ); 
-      if (r === true){ 
-        window.location.reload(); 
-      }
-   });
-  }
+      .catch((err) => {
+        setLoading(false)
+        let r = window.confirm(
+          "Face was not detected, press 'OK' to add new vivid picture."
+        );
+        if (r === true) {
+          window.location.reload();
+        }
+      });
+  };
 
-  const refreshPage = () => { 
-      window.location.reload(); 
-  }  
+  const refreshPage = () => {
+    window.location.reload();
+  };
 
-  const updateLoading = () => { 
-      setLoading(false); 
-  }
+  const updateLoading = () => {
+    setLoading(false);
+  };
 
   return (
     <div className="App">
-    {loading && <Spinner />}
-    <AddImage handleInputChange={handleInputChange} onSubmit={onSubmit}/>
-    <ModelImage imgUrl={imgUrl} box={box} /> 
-    {predictions.length !== 0 && <Popup  updateLoading={updateLoading} predictions={predictions}  refreshPage={refreshPage} />} 
+      {loading && <Spinner />}
+      <AddImage handleInputChange={handleInputChange} onSubmit={onSubmit} />
+      <ModelImage imgUrl={imgUrl} box={box} />
+      {predictions.length !== 0 && (
+        <Popup
+          updateLoading={updateLoading}
+          predictions={predictions}
+          refreshPage={refreshPage}
+        />
+      )}
     </div>
   );
 }
--- src/components/AddImage/AddImage.js
+++ src/components/AddImage/AddImage.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import './AddImage.css'
+import React from "react";
+import "./AddImage.css";
 
-const AddImage = ({handleInputChange, onSubmit}) => {
+const AddImage = ({ handleInputChange, onSubmit }) => {
 	return (
 		<div>
-		<p className='f2'>
-			{'Enter a profile picture & reveal the predicted demographic characteristics'}
-		</p>
-		<div className='center'>
-		 	<div className='form center pa4 br3 shadow-5'>
-			<input type='text' className='f4 pa2 w-70 center' placeholder="Enter Image Url" onChange={handleInputChange}/>
-			<button className='w-30 grow f4 link ph3 pv2 dib white bg-black' onClick={onSubmit} >Add Face</button>
+			<p className="f2">
+				{"Enter a celebrity profile picture to reveal his/her identity"}
+			</p>
+			<div className="center">
+				<div className="form center pa4 br3 shadow-5">
+					<input
+						type="text"
+						className="f4 pa2 w-70 center"
+						placeholder="Enter Image Url"
+						onChange={handleInputChange}
+					/>
+					<button
+						className="w-30 grow f4 link ph3 pv2 dib white bg-black"
+						onClick={onSubmit}
+					>
+						Predict
+					</button>
+				</div>
+			</div>
 		</div>
-		</div>
-		</div>
+	);
+};
 
-
-		
-	)
-}
-
-export default AddImage
+export default AddImage;
